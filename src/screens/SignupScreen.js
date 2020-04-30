10bbe2b605f665e34a33c3a86a0b99ec81ce1e50
Naoto_02_Suzuki <Naoto_02_Suzuki@n.t.rd.honda.co.jp>
2020-04-30 19:25:18 +0900

    Login, SIGNUP

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,16 +1,59 @@
 import React from 'react';
 import { StyleSheet, View, Text, TextInput, TouchableHighlight } from 'react-native';
+import firebase from 'firebase';
 
 class SignupScreen extends React.Component {
+  // state = {
+  //   email: '',
+  //   password: '',
+  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleSubmit() {
+    // Signup
+    firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
+      .then((user) => {
+        this.props.navigation.navigate('Home');
+      })
+      .catch((error) => {
+      // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode);
+      // ...
+      });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.title}>
           メンバー登録
         </Text>
-        <TextInput style={styles.input} value="Email Address" />
-        <TextInput style={styles.input} value="Password" />
-        <TouchableHighlight style={styles.button} underlayColor="#C70F66" onPress={() => {}}>
+        <TextInput
+          style={styles.input}
+          value={this.state.email}
+          onChangeText={(text) => { this.setState({ email: text }); }}
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Email Address"
+        />
+        <TextInput
+          style={styles.input}
+          value={this.state.password}
+          onChangeText={(text) => { this.setState({ password: text }); }}
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Password"
+          secureTextEntry
+        />
+        <TouchableHighlight style={styles.button} underlayColor="#C70F66" onPress={this.handleSubmit.bind(this)}>
           <Text style={styles.buttonTitle}>送信する</Text>
         </TouchableHighlight>
       </View>
